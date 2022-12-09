8570607d2c569dab3bc17a35c82ce7aec19e5aa6
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2022-12-10 00:31:12 +1300

    retrieve L1 db assessment work

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include "topbit.php";
+?>
+        
+        <div class="box main">
+            <h2>Food Review Database</h2>
+            
+            <p>
+                <br />Thank you for using my Orange Reviews website!
+            </p>
+            
+            <p>
+                Here you will be able to search food reviews by dish name, location, meal-time, rating and by vegetarian dishes. Please use the sidebar on the left to search for dishes. While creating this outcome, I didn't focus very much on the visual appeal of the front-end interface, as it's only a tool to display the content of my database and wasn't as essential as completing my queries correctly.
+            </p>
+            
+            <p>
+                There is currently no feedback form for this website, though chances are you are viewing this website per my request or to mark it as a teacher. Either way I'd love for you to get back to me with feedback (which I'm sure you'll have plenty of) on how to improve my website interface/data processing via my school email - unavailable here for security precautions.
+            </p>
+            
+        </div>    <!-- / main -->
+
+<?php
+    include "bottombit.php";
+?>
